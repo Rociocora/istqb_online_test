43aaad57b60881dd31b023696ae25da6d41cc328
Rociocora <161638863+Rociocora@users.noreply.github.com>
2024-05-13 21:15:30 +0200

    Welcome

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.write("HOLA MUNDO");
+
+function imprimir(mensaje, idElemento) {
+    document.getElementById(idElemento).innerText = mensaje;
+}
+
+
+function Nombre(){
+    //1.Obtener los valores del formulario. Para ello ponemos el id del elemento del formulario y y ponemos .value para obtener su valor y guardarlo en una variable.
+    let introduceNombre = document.getElementById("introduceNombre").value;
+    let mensaje = "Welcome " + introduceNombre + " to ISTQB online test";
+    imprimir(mensaje, "darBienvenida");
+}
+
+
