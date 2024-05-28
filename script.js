e2dff7587d1d1844b12064f735dea208db150708
Rociocora <161638863+Rociocora@users.noreply.github.com>
2024-05-28 20:22:41 +0200

    pruebas_proyecto

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+let mostrarDiezPreguntas = ["\n1.Which of the following is NOT a type of functional testing?",
+"\n2.What is the main objective of regression testing?",
+"\n3.Which test design technique is based on the requirements or functional specifications of the software?", 
+"\n4.Which type of testing focuses on the internal structure of the software?",  
+"\n5.Which of the following is NOT a static test design technique?",
+"\n6.What is the main objective of acceptance testing?",
+"\n7.Which testing is performed to determine how software behaves under extreme stress conditions?",
+"\n8.Which document specifies the steps needed to execute a set of tests?",
+"\n9.Which of the following is a fundamental principle of testing?",
+"\n10.Which type of testing is used to verify that the system meets legal and regulatory requirements?"]
+
+
+
 function imprimir(mensaje, idElemento) {
     document.getElementById(idElemento).innerText = mensaje;
 }
 
+function mostrarPreguntas() {
+    mensaje = mostrarDiezPreguntas;
+    imprimir(mensaje,"resultadoLista");
+}
 
 function nombre(){
     //1.Obtener los valores del formulario. Para ello ponemos el id del elemento del formulario y y ponemos .value para obtener su valor y guardarlo en una variable.
@@ -30,22 +47,31 @@ imprimir(mensaje,"respuestaCorrecta1");
 function corregirRespuesta(){
     let respuestaSeleccionada = document.getElementById("respuestasPregunta").value;
     if (respuestaSeleccionada === "respuestaC"){
-        mensaje = "The answer is correct.";
+        mensaje = "The answer is correct. Your score is 2.";
+    }
+    else if(respuestaSeleccionada === ""){
+        mensaje = "The anwer is not correct. Your score is 0."
+        
     }
     else{
-        mensaje = "The answer is not correct.";
+        mensaje = "The answer is not correct. Your score is -1.";
     }
 imprimir(mensaje,"respuestaCorrecta2");
 }
-function verPuntuacion(){
-    let respuestaSeleccionada = document.getElementById("respuestasPregunta").value;
-    if (respuestaSeleccionada === "respuestaC"){
-        mensaje = "Your score is 2.";
+
+function eligePregunta() {
+    let pregunta = document.getElementById("numeroPregunta").value;
+    let posicionPregunta = mostrarDiezPreguntas.indexOf(elemento);
+    let mensaje = ""
+    if(posicionElemento === -1){
+        mensaje = "El elemento no se encuentra en la lista";
+       
+
+    }else {
+        mensaje = "El elemento " + pregunta + " esta en la posicion con indice " + posicionPregunta;
     }
-    else if(respuestaSeleccionada ==="")
-        mensaje = "Your score is 0.";
-    else{
-        mensaje = "Your score is -1.";
-    }
-imprimir(mensaje,"respuestaCorrecta3");
+    imprimir(mensaje,"resultadoPregunta");
 }
+
+
+
